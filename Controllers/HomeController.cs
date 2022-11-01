82e927b5863cd365f7566208f0a02e3aae0d9506
joshua <joshuahsientan@gmail.com>
2022-10-31 19:09:39 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using APIRedditLabJT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace APIRedditLabJT.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        RedditAPIDAL api = new RedditAPIDAL();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListPosts()
+        {
+            RJSON sp = api.GetPost();
+            return View(sp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RedditAPIDAL.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace APIRedditLabJT.Models
+{
+    public class RedditAPIDAL
+    {
+        public RJSON GetPost()
+        {
+            var client = new RestClient($"https://www.reddit.com/r/aww/.json");
+            var request = new RestRequest();
+            var response = client.GetAsync<RJSON>(request);
+            RJSON rj = response.Result;
+            return rj;
+        }
+
+    }
+}
